7fbc737f4bd27ba7ada4d3c2f2857b091513e32e
Sanidhya Rajguru <84410647+Sanidhya572@users.noreply.github.com>
2024-04-10 17:53:38 +0530

    Created seperate files for each job and applied custom css to it.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,16 @@
-FROM python:3.8-alpine
-COPY  . /app
+FROM python:3.8
+
+# Set the working directory inside the container
 WORKDIR /app
-RUN pip install -r requirements.txt
-CMD python app.py
+
+# Copy the Flask application code into the container
+COPY . /app
+
+# Install dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Expose the port on which the Flask application will run
+EXPOSE 5000
+
+# Specify the command to run the Flask application
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -2,53 +2,51 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-
 JOBS = [
     {
-        'id':1,
+        'id': 1,
         'title': "Data Analyst",
         'Location': "Bengaluru, India",
         'Salary': 'Rs. 10,00,000'
     },
 
-        {
-        'id':2,
+    {
+        'id': 2,
         'title': "Data Scientist ",
         'Location': "Pune, India",
         'Salary': 'Rs. 12,00,000'
     },
 
-        {
-        'id':3,
+    {
+        'id': 3,
         'title': "Power BI Analyst",
         'Location': "Indore, India",
         'Salary': 'Rs. 5,00,000'
     },
 
-        {
-        'id':4,
+    {
+        'id': 4,
         'title': "Front End Developer",
         'Location': "Mumbai, India",
         'Salary': 'Rs. 11,00,000'
     },
 
-        {
-        'id':5,
+    {
+        'id': 5,
         'title': "Backend Developer",
         'Location': "Bengaluru, India",
         'Salary': 'Rs. 13,00,000'
     },
 
     {
-        'id':6,
+        'id': 6,
         'title': "UI/UX Designer",
         'Location': "Remote",
         'Salary': 'Rs. 7,00,000'
     },
 
-
-     {
-        'id':7,
+    {
+        'id': 7,
         'title': "Enterprise Architect",
         'Location': "Bengaluru, India",
         'Salary': 'Rs. 25,00,000'
@@ -63,7 +61,16 @@ def index():
 def list_jobs():
     return jsonify(JOBS)
 
-
+@app.route('/job/<int:job_id>')
+def job_details(job_id):
+    # Fetch job details based on job_id
+    # Example code to fetch job details from JOBS list:
+    job = next((job for job in JOBS if job['id'] == job_id), None)
+    if job:
+        return render_template(f'job_details_{job_id}.html', job=job)
+    else:
+        # Handle case where job is not found
+        return "Job not found", 404
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
